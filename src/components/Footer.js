b70de65c2c01807c8a1cd69a1c6fce36f09e0450
Barbara <barbara.estevez@gmail.com>
2021-06-02 13:02:54 +0200

    create Main, Form, CardPreview components. Delete files don't need. Copy header & footer scss

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,16 @@
 import React from "react";
+import "../stylesheet/_footer.scss";
 
 const Footer = () => {
   return (
-    <>
+    <footer className="footer">
       <small className="copy "> Awesome profile cards @ 2021 </small>
       <img
         className="logo-adalab"
         src="../images/logo-adalab.png"
         alt="logo logo-adalab"
       />
-    </>
+    </footer>
   );
 };
 export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import CardPreview from './CardPreview';
+import Form from './Form';
+
+function Main() {
+  return (
+    <>
+      <main className="main">
+        <CardPreview />
+        <Form />
+      </main>
+    </>
+  );
+}
+
+export default Main;
