6e40f08b7a34df6042b1c5389e2aafcd399360c7
EliaAR <alonsoramos.elia@gmail.com>
2021-06-12 11:40:04 +0200

    add footer component to landing

--- src/components/PageTwo.js
+++ src/components/PageTwo.js
@@ -1,13 +1,9 @@
+import React from "react";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
-import React from "react";
 
 class PageTwo extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     //Aquí se declaran las constantes(escribimos el valor entre paréntesis)
     return (
