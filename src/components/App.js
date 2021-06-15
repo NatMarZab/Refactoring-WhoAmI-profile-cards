a4749fffbc7780298b8167cc7f8c94212426a130
Barbara <barbara.estevez@gmail.com>
2021-06-15 10:39:20 +0200

    run docs2

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,30 @@ import "../stylesheet/App.scss";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Landing from "./Landing";
 import PageTwo from "./PageTwo";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
+
     <Router>
       <Switch>
-        <Route path="/" exact children={<Landing />} />
-        <Route path="/pagetwo" exact children={<PageTwo />} />
-        <Route children={<p>Página no encontrada</p>} />
+        <Route exact path="/">
+          <Landing />
+        </Route>
+
+        <Route exact path="/pagetwo">
+          <PageTwo />
+        </Route>
       </Switch>
     </Router>
+    // <Router>
+    //   <Switch>
+    //     <Route path="/" exact children={<Landing />} />
+    //     <Route path="/pagetwo" exact children={<PageTwo />} />
+    //     <Route children={<p>Página no encontrada</p>} />
+    //   </Switch>
+    // </Router>
   );
 }
 
